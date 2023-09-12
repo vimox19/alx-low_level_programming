bcd4a612ae412eadd6fde5c8ab8c42ccfa65e1af
vimox19 <sohaib.samadi@etu.uae.ac.ma>
2023-09-12 16:26:02 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-    	char str[] = "_putchar";
-	int ch;
 
-	for (ch = 0 ; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar('\n');
+printf("_putchar\n");
 
-   	 return (0);
+return (0);
 }
+
